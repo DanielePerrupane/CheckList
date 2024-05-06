1eb37d50c5e2754cf04ce6a62b7fc8c908aba7fa
DanielePerrupane <148336607+DanielePerrupane@users.noreply.github.com>
2024-05-06 09:57:12 +0200

    commit

--- CheckList/CheckList/View Controllers/ChecklistViewController.swift
+++ CheckList/CheckList/View Controllers/ChecklistViewController.swift
@@ -103,8 +103,8 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
         with item: ChecklistItem
     ){
         let label = cell.viewWithTag(1000) as! UILabel
-        //label.text = item.text
-        label.text = "\(item.itemID): \(item.text)"
+        label.text = item.text
+        //label.text = "\(item.itemID): \(item.text)"
     }
     
     
